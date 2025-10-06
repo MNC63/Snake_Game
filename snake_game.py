2bfb125c87400d241258768b84af006966a3248f
MNC63 <nloc8380@gmail.com>
2025-10-06 12:10:02 +0700

    done

--- snake_game.py
+++ snake_game.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((601, 601))
 pygame.display.set_caption('Snake')
 running = True
+pausing = False
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -13,10 +14,14 @@ ORANGE = (255, 165, 0)
 RED = (255, 0, 0)
 clock = pygame.time.Clock()
 
-snakes = [[9, 9], [9, 10], [9, 11]]
+snakes = [[5, 10]]
 direction = "right"
 
 apple = [randint(0, 19), randint(0, 19)]
+
+font_small = pygame.font.SysFont('sans', 20)
+font_big = pygame.font.SysFont('sans', 50)
+score = 0
 
 while running:
     clock.tick(60)
@@ -25,10 +30,10 @@ while running:
     tail_x = snakes[0][0]
     tail_y = snakes[0][1]
 
-    # Draw grid
-    for i in range(21):
-        pygame.draw.line(screen, GREEN, (0, i * 30), (600, i * 30))
-        pygame.draw.line(screen, GREEN, (i * 30, 0), (i * 30, 600))
+    # # Draw grid
+    # for i in range(21):
+    #     pygame.draw.line(screen, GREEN, (0, i * 30), (600, i * 30))
+    #     pygame.draw.line(screen, GREEN, (i * 30, 0), (i * 30, 600))
 
     # Draw Apple
     pygame.draw.rect(screen, RED, (apple[0]*30, apple[1]*30, 30, 30))
@@ -37,23 +42,48 @@ while running:
     if snakes[-1][0] == apple[0] and snakes[-1][1] == apple[1]:
         snakes.insert(0, [tail_x, tail_y])
         apple = [randint(0, 19), randint(0, 19)]
+        score += 1
 
     # Draw snake
     for snake in snakes:
         pygame.draw.rect(screen, ORANGE, (snake[0]*30, snake[1]*30, 30, 30))
 
-    if direction == "right":
-        snakes.append([snakes[-1][0] + 1, snakes[-1][1]])
-        snakes.pop(0)
-    if direction == "left":
-        snakes.append([snakes[-1][0] - 1, snakes[-1][1]])
-        snakes.pop(0)
-    if direction == "up":
-        snakes.append([snakes[-1][0], snakes[-1][1] - 1])
-        snakes.pop(0)
-    if direction == "down":
-        snakes.append([snakes[-1][0], snakes[-1][1] + 1])
-        snakes.pop(0)
+    # Check crash with edge
+    if snakes[-1][0] < 0 or snakes[-1][0] > 19 or snakes[-1][1] < 0 or snakes[-1][1] > 19:
+        pausing = True
+
+    # Draw Score:
+    score_text = font_small.render("Score: " + str(score), True, WHITE)
+    screen.blit(score_text, (5, 5))
+
+    # Snakes move
+    if pausing == False:
+        if direction == "right":
+            snakes.append([snakes[-1][0] + 1, snakes[-1][1]])
+            snakes.pop(0)
+        if direction == "left":
+            snakes.append([snakes[-1][0] - 1, snakes[-1][1]])
+            snakes.pop(0)
+        if direction == "up":
+            snakes.append([snakes[-1][0], snakes[-1][1] - 1])
+            snakes.pop(0)
+        if direction == "down":
+            snakes.append([snakes[-1][0], snakes[-1][1] + 1])
+            snakes.pop(0)
+
+    # Check crash with body
+    for i in range(len(snakes) - 1):
+        if snakes[-1][0] == snakes[i][0] and snakes[-1][1] == snakes[i][1]:
+            pausing = True
+
+    # Draw game over screen
+    if pausing:
+        game_over_txt = font_big.render(
+            "Game Over, Score: " + str(score), True, WHITE)
+        press_space_txt = font_big.render(
+            "Press space to continue", True, WHITE)
+        screen.blit(game_over_txt, (50, 200))
+        screen.blit(press_space_txt, (50, 250))
 
     sleep(0.1)
 
@@ -69,6 +99,11 @@ while running:
                 direction = "right"
             if event.key == pygame.K_a or event.key == pygame.K_LEFT and direction != "right":
                 direction = "left"
+            if event.key == pygame.K_SPACE and pausing == True:
+                pausing = False
+                score = 0
+                snakes = [[5, 10]]
+                apple = [randint(0, 19), randint(0, 19)]
     pygame.display.flip()
 
 pygame.quit()
